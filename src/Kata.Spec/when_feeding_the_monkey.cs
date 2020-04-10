420ef197846856e191197d8db5f7a3cbd9a4f9a6
Jose Rodolfo Martinez Bardales <rodolfojose@live.com>
2020-04-10 14:47:01 -0700

    Unkown Amount

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -58,6 +58,21 @@ namespace Kata.Spec
         static Calculator _systemUnderTest;
         static int _result;
     }
+
+    public class unkownAmount
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("1,2,3"); };
+
+        It should_do_something = () => { _result.Should().Be(6); };
+        private static Calculator _systemUnderTest;
+        private static int _result;
+    }
+
     // Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
     // Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
     // Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,12 +1,19 @@
+using System;
+
 namespace Kata.Spec
 {
     public class Calculator
     {
         public int Add(string s ="")
         {
+            string[] pieces = s.Split(',');
             if (string.IsNullOrEmpty(s))
                 return 0;
-            return 1;
+            if (pieces.Length == 1)
+            {
+                return int.Parse(pieces[0]);
+            }
+            return int.Parse(pieces[0]) + int.Parse(pieces[1]);
         }
     }
 }
